33f356aa7195ba51c454fe709e98af84da021c11
Addison580 <82746288+Addison580@users.noreply.github.com>
2022-11-19 19:00:28 +0530

    Add files via upload

---
+++ laptop.js
@@ -0,0 +1,14 @@
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+
+}
+
+function preload(){
+img = loadImage('laptop.jpg');
+
+}
+
+function draw(){
+image(img, 0, 40, 540, 320);
+}
